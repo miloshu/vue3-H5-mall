c51ae4a7be993b0f7b5bafb3d8afe74257bf51df
miloshu <454924687@qq.com>
2020-12-28 23:37:39 +0800

    商品分类页ts类型定义

--- src/views/ProductList/index.vue
+++ src/views/ProductList/index.vue
@@ -40,40 +40,30 @@
 <script lang="ts">
 import { onMounted, reactive, toRefs, defineComponent } from 'vue'
 import { getCategoriesList } from '/@/api/category'
+// 类型定义
+interface stateProps {
+  searchVal: string;
+  activeId: number;
+  activeIndex: number;
+  leftMenuList: object[];
+  rightConList: object[];
+}
 export default defineComponent({
   setup () {
-    const state = reactive({
+    const state: stateProps = reactive({
       searchVal: '',
       activeId: 1,
       activeIndex: 0,
-      lists: [],
       leftMenuList: [],
       rightConList: []
     });
-    const items = [
-      {
-        text: '浙江',
-        children: [
-          { text: '杭州', id: 1 },
-          { text: '温州', id: 2 },
-        ],
-      },
-      {
-        text: '江苏',
-        children: [
-          { text: '南京', id: 5 },
-          { text: '无锡', id: 6 },
-        ],
-      },
-    ]
     const methods = reactive({
-      getCategoriesList: async () => {
+      getCategoriesList: async () =>  {
         try {
           const res = await getCategoriesList({})
-          state.lists = res.data.message
-          state.leftMenuList = res.data.message.filter(v => v.cat_name)
-          state.rightConList = res.data.message[0].children
-          // state.rightConList = res.data.message[0].children
+          const lists = res.data.message
+          state.leftMenuList = lists.filter(v => v.cat_name)
+          state.rightConList = lists[0].children
           console.log(res.data.message[0].children, '999999')
         } catch (error) {}
       },
